2ba39b19642d98a8ec367bd53f5e247d2b0d4401
13519001 Karlsen Adiyasa Bachtiar <13519001@std.stei.itb.ac.id>
2022-05-14 11:46:00 +0700

    get raw json file from server

--- javascript/number_1/index.js
+++ javascript/number_1/index.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
+const axios = require('axios')
 const express = require('express');
 const app = express();
 
 function getJsonData(filename) {
+    // Get json data from local file
     let data = [];
     let rawdata = fs.readFileSync(filename);
     let parsedData = JSON.parse(rawdata);
@@ -27,14 +29,25 @@ function parseData(data) {
     return myObj;
 }
 
-
-const data = getJsonData("data.json");
-const parsedData = parseData(data);
-
-app.get('/getParsedData', function (req, res) {
-    res.json(parsedData);
+app.get('/getRawData', function (req, res) {
+    const data = getJsonData("data.json");
+    res.json(data);
 });
 
+app.get('/getParsedData', function (req, res) {
+    axios
+        .get("http://localhost:3000/getRawData")
+        .then(function (response) {
+            // to get data from a local file use getJsonData(filename)
+            // const data = getJsonData("data.json");
+            const data = response.data;
+            const parsedData = parseData(data);
+            res.json(parsedData);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+});
 
 app.listen(3000, function (req, res) {
     console.log("Server is running at port 3000");
--- javascript/number_1/number_1_guide.md
+++ javascript/number_1/number_1_guide.md
@@ -2,6 +2,7 @@
 ```
 npm install fs
 npm install express
+npm install axios
 ```
 
 ## Run server using command :
@@ -10,6 +11,10 @@ npm install express
 node index.js
 ```
 
-## Open link
+## Get Raw Data
+
+<a href="http://localhost:3000/getRawData">http://localhost:3000/getRawData</a>
+
+## Get Parsed Data
 
 <a href="http://localhost:3000/getParsedData">http://localhost:3000/getParsedData</a>
